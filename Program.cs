acd05ecc2d175be885237f1c220ab9c5e0df8e72
Rico Rodriguez <97664519+rico-rodriguez@users.noreply.github.com>
2022-10-15 15:35:08 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿//Random name selector app
+//Created by: Rico Rodriguez
+//Created on: 2022-10-15
+//Description: This program will randomly select a name from a list of names
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variables
+            string[] names = { "Rico", "John", "Bob", "Sally", "Jane", "Mike", "Sue", "Joe", "Mary", "Sara", "Beth" };
+            int randomNum;
+            Random rnd = new Random();
+
+            //Process
+            randomNum = rnd.Next(0, names.Length);
+
+            //Output
+            Console.WriteLine("The winner is " + names[randomNum]);
+        }
+    }
+}
